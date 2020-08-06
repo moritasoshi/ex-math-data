f88120aadb8f448e8c65d073d67735e7f488cc72
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-08-06 09:30:31 +0900

    first commit

---
+++ src/main/java/com/example/ExMathDataApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExMathDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExMathDataApplication.class, args);
+	}
+
+}
