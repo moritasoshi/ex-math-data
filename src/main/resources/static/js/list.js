d3ab496a5e20c848b45645557f429eaa1f0ec729
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-08-07 17:40:29 +0900

    検索カテゴリを自動表示

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -6,9 +6,11 @@ $(function() {
 	// 	xhr.setRequestHeader(header, token);
 	// });
 
-	// 親カテゴリのセットアップ
+	//// 画面描画直後に親カテゴリのセレクトボックスを自動生成
+	// 親カテゴリ
 	setUpParentCategory();
 
+	//// 動的にカテゴリのセレクトボックスを変化させる機能
 	// 子カテゴリのセットアップ
 	$('.parent-category').change(function() {
 		$('.child-category > option').remove();
@@ -34,8 +36,17 @@ $(function() {
 		})
 			.done(function(parentList) {
 				const target = $('.parent-category');
+				const parentSelected = $('input:hidden[name="parentCategory"]').val();
+				const childSelected = $('input:hidden[name="childCategory"]').val();
+				const grandChildSelected = $('input:hidden[name="grandChildCategory"]').val();
 				for (const parent of parentList) {
-					target.append($('<option>').html(parent).val(parent));
+					if (parent == parentSelected) {
+						// 検索結果を表示させてる場合はセレクトボックスにカテゴリを表示
+						target.append($('<option>').html(parent).val(parent).prop('selected',true));
+						setUpChildCategoryS(parentSelected, childSelected);
+					} else {
+						target.append($('<option>').html(parent).val(parent));
+					}
 				}
 			})
 			.fail(function(XMLHttpRequest, textStatus, errorThrown) {
@@ -64,6 +75,34 @@ $(function() {
 				alert('リクエストに失敗' + textStatus + ':\n' + errorThrown);
 			});
 	}
+	//// 子カテゴリを自動生成するメソッド（検索結果を表示している場合）
+	function setUpChildCategoryS(parentSelected, childSelected) {
+		const parent = parentSelected;
+		const grandChildSelected = $('input:hidden[name="grandChildCategory"]').val();
+		$.ajax({
+			type: 'POST',
+			url: '/ajax/setUpChildCategory',
+			data: {
+				parent: parent
+			},
+			dataType: 'json'
+		})
+			.done(function(childList) {
+				const target = $('.child-category');
+				for (const child of childList) {
+					if (child == childSelected) {
+						// 検索結果を表示させてる場合はセレクトボックスにカテゴリを表示
+						target.append($('<option>').html(child).val(child).prop('selected',true));
+						setUpGrandChildCategoryS(parentSelected, childSelected, grandChildSelected);
+					} else {
+						target.append($('<option>').html(child).val(child));
+					}
+				}
+			})
+			.fail(function(XMLHttpRequest, textStatus, errorThrown) {
+				alert('リクエストに失敗' + textStatus + ':\n' + errorThrown);
+			});
+	}
 	//// 孫カテゴリを自動生成するメソッド
 	function setUpGrandChildCategory() {
 		const parent = $('.parent-category').val();
@@ -81,6 +120,34 @@ $(function() {
 				const target = $('.grandChild-category');
 				for (const grandChild of grandChildList) {
 					target.append($('<option>').html(grandChild).val(grandChild));
+				}
+			})
+			.fail(function(XMLHttpRequest, textStatus, errorThrown) {
+				alert('リクエストに失敗' + textStatus + ':\n' + errorThrown);
+			});
+	}
+	//// 孫カテゴリを自動生成するメソッド（検索結果を表示している場合）
+	function setUpGrandChildCategoryS(parentSelected, childSelected, grandChildSelected) {
+		const parent = parentSelected;
+		const child = childSelected;
+		$.ajax({
+			type: 'POST',
+			url: '/ajax/setUpGrandChildCategory',
+			data: {
+				parent: parent,
+				child: child
+			},
+			dataType: 'json'
+		})
+			.done(function(grandChildList) {
+				const target = $('.grandChild-category');
+				for (const grandChild of grandChildList) {
+					if (grandChild == grandChildSelected) {
+						// 検索結果を表示させてる場合はセレクトボックスにカテゴリを表示
+						target.append($('<option>').html(grandChild).val(grandChild).prop('selected',true));
+					} else {
+						target.append($('<option>').html(grandChild).val(grandChild));
+					}
 				}
 			})
 			.fail(function(XMLHttpRequest, textStatus, errorThrown) {
